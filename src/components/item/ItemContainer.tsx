e2ce7a58bbd5e85028d5b2fed7404dbb2ea3863c
Kimbangul <highcolor_12@naver.com>
2023-05-17 09:07:31 +0900

    feat: add connection

--- src/components/item/ItemContainer.tsx
+++ src/components/item/ItemContainer.tsx
@@ -1,17 +1,23 @@
 'use client';
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import { useParams } from "next/navigation";
 import client from "@/axios/client";
 import ItemView from "@/components/item/ItemView";
 
 const ItemContainer = () => {
   const router = useRouter();
-
+  const params = useSearchParams();
+  const params2 = useParams();
+  const pathName = usePathname();
+  console.log(process.env.NEXT_PUBLIC_API_URL);
   useEffect(()=>{ 
-    //const data = client.get('/?url=https://velog.io/@kimbangul');
-    //console.log(data);
-    console.log(router.query);
+    const id = params.get('id');
+    console.log(id);
+    const data = client.get(`/?id=${id}`);
+
+    console.log(data);
   }, []);
   
   return(
